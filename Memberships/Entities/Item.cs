1d126eaa824936d6ccc207519d9f85703c64a3c5
AsiaC <czaplicka.asia@gmail.com>
2018-07-11 15:25:35 +0200

    na widoku create Item zmania isFree na checkedbox

--- Memberships/Entities/Item.cs
+++ Memberships/Entities/Item.cs
@@ -34,7 +34,7 @@ namespace Memberships.Entities
         public int ItemTypeId { get; set; }
         public int SectionId { get; set; }
         public int PartId { get; set; }
-        public int IsFree { get; set; }
+        public bool IsFree { get; set; }
         [DisplayName("Item Type")]
         public ICollection<ItemType> ItemTypes { get; set; }
         [DisplayName("Sections")]
